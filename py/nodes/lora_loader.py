70de5ee3765bbf77251340ebc7b19b6375876821
thr12345 <thr12345thr12345@gmail.com>
2025-06-08 20:38:46 -0400

    Working loader with nunchaku

--- py/nodes/lora_loader.py
+++ py/nodes/lora_loader.py
@@ -1,10 +1,43 @@
 import logging
-from nodes import LoraLoader
+import copy
+import folder_paths
 from comfy.comfy_types import IO # type: ignore
 import asyncio
 from .utils import FlexibleOptionalInputType, any_type, get_lora_info, extract_lora_name, get_loras_list
+from nunchaku.lora.flux import to_diffusers
 
 logger = logging.getLogger(__name__)
+
+def load_lora(model, lora_name: str, lora_strength: float):
+    if abs(lora_strength) < 1e-5:
+        return (model)
+    model_wrapper = model.model.diffusion_model
+    # assert isinstance(model_wrapper, ComfyFluxWrapper)
+
+    transformer = model_wrapper.model
+    model_wrapper.model = None
+    ret_model = copy.deepcopy(model)  # copy everything except the model
+    ret_model_wrapper = ret_model.model.diffusion_model
+    # assert isinstance(ret_model_wrapper, ComfyFluxWrapper)
+
+    model_wrapper.model = transformer
+    ret_model_wrapper.model = transformer
+
+    lora_path = folder_paths.get_full_path_or_raise("loras", lora_name)
+    ret_model_wrapper.loras.append((lora_path, lora_strength))
+
+    sd = to_diffusers(lora_path)
+
+    if "transformer.x_embedder.lora_A.weight" in sd:
+        new_in_channels = sd["transformer.x_embedder.lora_A.weight"].shape[1]
+        assert new_in_channels % 4 == 0
+        new_in_channels = new_in_channels // 4
+
+        old_in_channels = ret_model.model.model_config.unet_config["in_channels"]
+        if old_in_channels < new_in_channels:
+            ret_model.model.model_config.unet_config["in_channels"] = new_in_channels
+
+    return (ret_model)
 
 class LoraManagerLoader:
     NAME = "Lora Loader (LoraManager)"
@@ -15,7 +48,7 @@ class LoraManagerLoader:
         return {
             "required": {
                 "model": ("MODEL",),
-                # "clip": ("CLIP",),
+                "clip": ("CLIP",),
                 "text": (IO.STRING, {
                     "multiline": True, 
                     "dynamicPrompts": True, 
@@ -36,23 +69,22 @@ class LoraManagerLoader:
         all_trigger_words = []
         
         clip = kwargs.get('clip', None)
-        lora_stack = kwargs.get('lora_stack', None)
         # First process lora_stack if available
-        if lora_stack:
-            for lora_path, model_strength, clip_strength in lora_stack:
-                # Apply the LoRA using the provided path and strengths
-                model, clip = LoraLoader().load_lora(model, clip, lora_path, model_strength, clip_strength)
+        # if lora_stack:
+        #     for lora_path, model_strength, clip_strength in lora_stack:
+        #         # Apply the LoRA using the provided path and strengths
+        #         model, clip = LoraLoader().load_lora(model, clip, lora_path, model_strength, clip_strength)
                 
-                # Extract lora name for trigger words lookup
-                lora_name = extract_lora_name(lora_path)
-                _, trigger_words = asyncio.run(get_lora_info(lora_name))
+        #         # Extract lora name for trigger words lookup
+        #         lora_name = extract_lora_name(lora_path)
+        #         _, trigger_words = asyncio.run(get_lora_info(lora_name))
                 
-                all_trigger_words.extend(trigger_words)
-                # Add clip strength to output if different from model strength
-                if abs(model_strength - clip_strength) > 0.001:
-                    loaded_loras.append(f"{lora_name}: {model_strength},{clip_strength}")
-                else:
-                    loaded_loras.append(f"{lora_name}: {model_strength}")
+        #         all_trigger_words.extend(trigger_words)
+        #         # Add clip strength to output if different from model strength
+        #         if abs(model_strength - clip_strength) > 0.001:
+        #             loaded_loras.append(f"{lora_name}: {model_strength},{clip_strength}")
+        #         else:
+        #             loaded_loras.append(f"{lora_name}: {model_strength}")
         
         # Then process loras from kwargs with support for both old and new formats
         loras_list = get_loras_list(kwargs)
@@ -69,7 +101,7 @@ class LoraManagerLoader:
             lora_path, trigger_words = asyncio.run(get_lora_info(lora_name))
             
             # Apply the LoRA using the resolved path with separate strengths
-            model, clip = LoraLoader().load_lora(model, clip, lora_path, model_strength, clip_strength)
+            model = load_lora(model, lora_path, model_strength)
             
             # Include clip strength in output if different from model strength
             if abs(model_strength - clip_strength) > 0.001:
